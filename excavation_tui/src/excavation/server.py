789ad04094928dafe99008f1d0815e2d6cb261c0
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-02-08 12:39:58 +0530

    (v0.1.1.5-alpha) Revert Deployement
    Scratch that, it doesnt work with render :sob:

--- excavation_tui/src/excavation/server.py
+++
@@ -1,9 +0,0 @@
-import os
-from textual_serve.server import Server
-
-server = Server(
-    command = "python -m textual",
-    host = "0.0.0.0",
-    port = int(os.environ.get("PORT", 10000)),
-)
-server.serve()
